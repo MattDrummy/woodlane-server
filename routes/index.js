ee8fab2111f706048062ae6657d5d79c998d4f50
Matthew Drumheller <MattDrummy@gmail.com>
2017-09-01 22:31:29 -0600

    finished all routes

--- routes/index.js
+++ routes/index.js
@@ -13,19 +13,63 @@ router.get('/:table', (req, res, next) => {
 })
 
 router.post('/:table', (req, res, next) => {
-  if (!req.body.post) {
+  if (JSON.stringify(req.body) == JSON.stringify({})) {
     res.json({
-      message: 'Sorry, you did not include a proper post'
+      message: 'Sorry, you did not include a proper post',
+      body: req.body
     })
+  } else {
+    knex(req.params.table)
+      .insert(req.body)
+      .returning('*')
+      .then((data) => {
+        return knex(req.params.table)
+      })
+      .then((data)=> {
+        res.json(data)
+      })
+      .catch((err) => {
+        res.json(err)
+      })
   }
 })
 
 router.put('/:table/:id', (req, res, next) => {
-
+  if (JSON.stringify(req.body) == JSON.stringify({})) {
+    res.json({
+      message: 'Sorry, you did not include a proper update',
+      body: req.body
+    })
+  } else {
+    knex(req.params.table)
+      .where('id', req.params.id)
+      .update(req.body)
+      .returning('*')
+      .then((data) => {
+        return knex(req.params.table)
+      })
+      .then((data)=> {
+        res.json(data)
+      })
+      .catch((err) => {
+        res.json(err)
+      })
+  }
 })
 
 router.delete('/:table/:id', (req, res, next) => {
-
+  knex(req.params.table)
+    .where('id', req.params.id)
+    .del()
+    .then((data) => {
+      return knex(req.params.table)
+    })
+    .then((data)=> {
+      res.json(data)
+    })
+    .catch((err) => {
+      res.json(err)
+    })
 })
 
 module.exports = router;
